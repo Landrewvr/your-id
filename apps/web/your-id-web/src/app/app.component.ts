7b759934a8bf70f9cec6d9ce7b69cde6eac1fbba
Landrewvr <luis.andrew.veras@gmail.com>
2024-11-24 05:27:28 -0500

    ui version 1

--- apps/web/your-id-web/src/app/app.component.ts
+++ apps/web/your-id-web/src/app/app.component.ts
@@ -1,27 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { faBars, faCheck, faPen, faUser, } from '@fortawesome/free-solid-svg-icons';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
-import { decrypt, encrypt } from './test';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
-  title = 'your-id-web';
-  icons = {
-    faBars: faBars,
-    faPen: faPen,
-    faCheck: faCheck,
-    faUser: faUser
-  }
+export class AppComponent {
+  faBars = faBars;
 
-  ngOnInit(): void {
-    const test = encrypt('2FS5-RE1-SF06',6);
-    console.log(test);
-    console.log(decrypt('7KM:3ZK;3SQ=9',6));
-  }
+  // ngOnInit(): void {
+  //   const test = encrypt('2FS5-RE1-SF06',6);
+  //   console.log(test);
+  //   console.log(decrypt('7KM:3ZK;3SQ=9',6));
+  // }
 }
