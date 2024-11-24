a44ded029e7877563053974a58c510b857f9f781
Landrewvr <luis.andrew.veras@gmail.com>
2024-11-24 13:57:54 -0500

    Progress on Your id service

--- apps/services/your-id-service/src/index.ts
+++ apps/services/your-id-service/src/index.ts
@@ -1,4 +1,39 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+// Initialize doteng to load env variables
+dotenv.config();
+
+// Express app creation
+const app = express();
+
+// Pulling enviroment constants from .env file
+const appPort = process.env.DEV_APP_PORT || '';
+const dbUrl = process.env.DATABASE_URL || '';
+
+mongoose.set('strictQuery', false);
+mongoose.connect(dbUrl)
+.then((db) => {
+    console.log(`Connected to ${db.connection.name} database on ${db.connection.host}:${db.connection.port}`);
+})
+.catch((error: any) => {
+    console.log('Database connection error:', error);
+});
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+
+// const dummyUserRouter = require('./routes/dummyUser')
+// app.use('/dummyUser', dummyUserRouter)
+
+// Server Start
+app.listen(appPort, () => {
+    console.log(`Server is running on localhost:${appPort}`)
+})
 
 console.log('test test')
